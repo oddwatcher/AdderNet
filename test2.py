55e3a8bc434d55f5c6549e91c7e80c87b9f82dd9
oddwatcher <scien@4090>
2025-07-23 08:38:19 +0000

    removed unusd files

--- test2.py
+++
@@ -1,8 +0,0 @@
-import torch
-from resnet20 import resnet20
-
-model = resnet20().cuda()
-saved_state_dict = torch.load('addernet_final.pt')
-model.load_state_dict(torch.load('addernet_final.pt'))
-
-model.eval()
